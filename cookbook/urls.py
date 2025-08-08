e068e9a1ffd6ce8c76f44e7a50c77d11a18a9371
Brent Walter <bew123@gmail.com>
2025-08-08 01:48:40 -0700

    Add URL pattern to serve media files in production

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -1,26 +1,9 @@
-"""
-URL configuration for cookbook project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# FILE: cookbook/urls.py
+# cookbook/urls.py
 
 from django.contrib import admin
 from django.urls import path, include
 
-# These two imports are required for this to work!
+# These two imports are essential for serving files in production
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -29,6 +12,5 @@ urlpatterns = [
     path('', include('recipes.urls')),
 ]
 
-# This line tells Django how to serve media files in production.
-# It MUST be present and NOT wrapped in an 'if' statement.
+# This line is the fix. It adds a URL pattern for your media files.
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
